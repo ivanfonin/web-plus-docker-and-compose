c2bcc6f9e6b48898a1a9a975f93878c044dc90ad
Ivan Fonin <ivanfonin@yandex.ru>
2024-09-15 11:02:27 +0300

    feat: Добавить переменную окружения REACT_APP_BACKEND_URL в frontend, чтобы в ней задавать адрес API, скорректировать README.md

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://api.ivanfonin.nomorepartiesco.ru"; // "http://localhost:4000"
+export const URL = process.env.REACT_APP_BACKEND_URL || "https://api.ivanfonin.nomorepartiesco.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
